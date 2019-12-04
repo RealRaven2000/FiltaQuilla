5fa0620a416fc1f94265713f0ce1c742dd415fb4
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-12-04 20:17:11 +0100

    build scripts

--- build.bat
+++ build.bat
@@ -1,9 +1,5 @@
-set /P FiltaQuillaRev=<revision.txt
-set /a oldRev=%FiltaQuillaRev%
-set /a FiltaQuillaRev+=1
-#pwsh -Command "(gc -en UTF8NoBOM install.rdf) -replace 'pre%oldRev%', 'pre%FiltaQuillaRev%' | Out-File install.rdf"
-pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%', 'pre%FiltaQuillaRev%' | Out-File manifest.json"
-"C:\Program Files\7-Zip\7z" a -xr!.svn FiltaQuilla.zip install.rdf manifest.json chrome.manifest content defaults locale skin license.txt
-echo %FiltaQuillaRev% > revision.txt
-move *.xpi "..\..\_Test Versions\2.0\"
-rename FiltaQuilla.zip FiltaQuilla-2.0pre%FiltaQuillaRev%.xpi
+@echo off
+setlocal
+
+set MYDIR=%~dp0
+powershell -File "%MYDIR%\build.ps1"
---
+++ revision.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+
+set MYDIR=%~dp0
+set BKPDIR=%MYDIR%\..\..\_Test Versions\2.0\
+
+move *.xpi "%BKPDIR%">NUL
+powershell -Version 3 -File "%MYDIR%\build.ps1" -IncrementRevision
