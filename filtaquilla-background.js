b52268c4d83d23cf85dfd5484ad5c9b92146107d
RealRaven2000 <axel.grude@gmail.com>
2021-07-11 21:37:53 +0100

    consolidated locales for mx conversion

--- filtaquilla-background.js
+++ filtaquilla-background.js
@@ -10,8 +10,8 @@
       ["resource", "filtaquilla",           "content/"],  
       ["resource", "filtaquilla-skin",      "skin/"],  // make a separate resource (we can't have 2 different resources mapped to to the same name)
       ["content",  "filtaquilla",           "content/"],
-      ["locale",   "filtaquilla", "en",     "locale/en-US/"],
-      ["locale",   "filtaquilla", "sv",     "locale/sv-SE/"],
+      ["locale",   "filtaquilla", "en",     "locale/en/"],
+      ["locale",   "filtaquilla", "sv",     "locale/sv/"],
       ["locale",   "filtaquilla", "de",     "locale/de/"],
       ["locale",   "filtaquilla", "nl",     "locale/nl/"],
       ["locale",   "filtaquilla", "ru",     "locale/ru/"]
