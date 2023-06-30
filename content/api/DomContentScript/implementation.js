f4a3d55e1c9f4275538edb6fd7760af8aa130fff
Tooru Fujisawa <arai_a@mac.com>
2023-07-01 03:12:33 +0900

    Use Services global variable if possible
    Services.jsm is planned to be removed in Firefox 117 cycle in https://bugzilla.mozilla.org/show_bug.cgi?id=1780695 .
    Services global variable is available in WebExtensions experiments API global
    from version 88 https://bugzilla.mozilla.org/show_bug.cgi?id=1698158 ,
    and also available in all system globals from version 104
    https://bugzilla.mozilla.org/show_bug.cgi?id=1667455 ,
    and those code doesn't have to import Services.jsm for recent versions.

--- content/api/DomContentScript/implementation.js
+++ content/api/DomContentScript/implementation.js
@@ -5,7 +5,9 @@ var { ExtensionCommon } = ChromeUtils.import(
 var { ExtensionUtils } = ChromeUtils.import(
   "resource://gre/modules/ExtensionUtils.jsm"
 );
-var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+var Services = globalThis.Services || ChromeUtils.import(
+  "resource://gre/modules/Services.jsm"
+).Services;
 
 var { ExtensionError } = ExtensionUtils;
 
--- content/fq_FilterEditor.js
+++ content/fq_FilterEditor.js
@@ -20,7 +20,9 @@
  */
 
 {
-  var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+  var Services = globalThis.Services || ChromeUtils.import(
+    "resource://gre/modules/Services.jsm"
+  ).Services;
   Services.scriptloader.loadSubScript("chrome://filtaquilla/content/filtaquilla-util.js") // FiltaQuilla object
 
   const util = FiltaQuilla.Util,
