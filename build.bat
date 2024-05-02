ada1b7f3c91cbb8ca5accc3f2484eb1146005305
RealRaven2000 <axel.grude@gmail.com>
2024-05-02 21:26:28 +0100

    4.1

--- build.bat
+++ build.bat
@@ -6,6 +6,6 @@ REM replace previous rev with new
 powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%filtquillaRev%' | Out-File manifest.json  -encoding utf8"
 "C:\Program Files\7-Zip\7z" a -xr!.svn filtaQuilla-FX.zip manifest.json _locales content defaults locale skin license.txt *.js *.html
 echo %filtquillaRev% > revision.txt
-move filtaQuilla*.xpi "..\..\..\_Test Versions\4.0\"
+move filtaQuilla*.xpi "..\..\..\_Test Versions\4.1\"
 powershell -Command "Start-Sleep -m 150"
-rename filtaQuilla-FX.zip filtaquilla-4.0pre%filtquillaRev%.xpi
+rename filtaQuilla-FX.zip filtaquilla-4.1pre%filtquillaRev%.xpi
