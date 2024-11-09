29fc521df82181da5c9d515f973891dd0045ad5c
RealRaven2000 <axel.grude@gmail.com>
2024-11-09 09:41:06 +0000

    4.2.1

--- build.bat
+++ build.bat
@@ -8,4 +8,4 @@ powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%fi
 echo %filtquillaRev% > revision.txt
 move filtaQuilla*.xpi "..\..\..\_Test Versions\4.2\"
 powershell -Command "Start-Sleep -m 150"
-rename filtaQuilla-FX.zip filtaquilla-4.2pre%filtquillaRev%.xpi
+rename filtaQuilla-FX.zip filtaquilla-4.2.1pre%filtquillaRev%.xpi
