0791ac016be304b9d4ba5a90e1a6c9346c8c831d
RealRaven2000 <axel.grude@gmail.com>
2025-06-12 12:07:43 +0100

    added ES linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2024: true,
+    node: true,
+    webextensions: true,
+  },
+  globals: {
+    browser: "readonly",
+    messenger: "readonly",
+    ChromeUtils: "readonly",
+    PathUtils: "readonly",
+    Services: "readonly",
+    FiltaQuilla: "readonly",
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  rules: {
+    // your rules here
+    "no-const-assign": "error",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-undef": "error",
+    "no-redeclare": "error",
+    eqeqeq: "off",
+    curly: "warn",
+  },
+};
